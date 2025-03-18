d6e85eb9f46b6208fdb385a2e07dd3e7f46dd0e9
PinzaF1 <carlosdanielpinzadiaz@gmail.com>
2025-03-17 23:24:30 -0500

    SUBIDA DE ARCHIVOS

---
+++ pinza/Inicio_sesion_admi.java
@@ -0,0 +1,21 @@
+package com.example.icfes;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Inicio_sesion_admi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion_usuario);
+
+        Button btnInicoSecion = findViewById(R.id.btnInicoSecion);
+    }
+}
